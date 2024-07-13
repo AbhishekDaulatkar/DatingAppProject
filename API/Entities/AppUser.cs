afb75df6ab4d2201820bb7ec131f9b87294968e3
AbhishekDaulatkar <abhishekdaulatkar@gmail.com>
2024-07-13 23:10:21 +0530

    End Of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,12 @@
 ﻿namespace API.Entities;
 
-public class AppUser
+public class AppUser //represents database, not a database name
 {
-    public int Id { get; set; }
+    public int Id { get; set; } // actual columns in database
+
     public string UserName { get; set; }
+
+    public byte[] PasswordHash { get; set; }
+
+    public byte[] PasswordSalt { get; set; }
 }
